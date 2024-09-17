3cc6822c3648ec3ec4368f9bf54077aee66b0ce1
Akshay <akshaynighot73@gmail.com>
2024-09-17 18:38:23 +0530

    view model logic updated for placeholder zero value

--- app/src/main/java/com/absut/cashcalculator/MainViewModel.kt
+++ app/src/main/java/com/absut/cashcalculator/MainViewModel.kt
@@ -8,27 +8,28 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     val denominations = listOf(2000, 500, 200, 100, 50, 20, 10)
-    var counts = mutableStateListOf<Int>()
+    var counts = mutableStateListOf<String>()
     var total by mutableIntStateOf(0)
     var totalNotes by mutableIntStateOf(0)
 
     init {
-        counts.addAll(List(denominations.size) { 0 })
+        //counts.addAll(List(denominations.size) { 0 })
+        resetCalculator()
     }
 
-    fun updateCount(index: Int, count: Int) {
+    fun updateCount(index: Int, count: String) {
         counts[index] = count
         calculateTotal()
     }
 
     private fun calculateTotal() {
-        total = denominations.zip(counts).sumOf { (denom, count) -> denom * count }
-        totalNotes = counts.sum()
+        total = denominations.zip(counts).sumOf { (denom, count) -> denom * (count.toIntOrNull() ?: 0) }
+        totalNotes = counts.sumOf { it.toIntOrNull() ?: 0 }
     }
 
     fun resetCalculator() {
         counts.clear()
-        counts.addAll(List(denominations.size) { 0 })
+        counts.addAll(List(denominations.size) { "" })
         total = 0
         totalNotes = 0
     }
