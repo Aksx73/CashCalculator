af7126ab4f741dea5d4d6c51b696382a29948e7d
Akshay <akshaynighot73@gmail.com>
2024-09-29 17:34:31 +0530

    Repository implemented

---
+++ app/src/main/java/com/absut/cashcalculator/data/repository/CashRecordRepository.kt
@@ -0,0 +1,17 @@
+package com.absut.cashcalculator.data.repository
+
+import android.content.Context
+import com.absut.cashcalculator.data.local.RecordsDatabase
+import com.absut.cashcalculator.data.model.CashRecord
+
+class CashRecordRepository(private val context: Context) {
+
+    private val recordsDao = RecordsDatabase.getDatabase(context).getRecordsDao()
+
+    suspend fun saveCashRecord(cashRecord: CashRecord) {
+        recordsDao.saveCashRecord(cashRecord)
+    }
+
+    fun getSavedRecords() = recordsDao.getSavedRecords()
+
+}
