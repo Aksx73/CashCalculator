3f75a4d0f0d65b4118ca6329ba2b1aeed79659df
Akshay <akshaynighot73@gmail.com>
2024-11-04 09:40:12 +0530

    upgraded AGP version to 8.9 and compile&target sdk version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.absut.cashcalculator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.absut.cashcalculator"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 3
         versionName = "1.2"
 
